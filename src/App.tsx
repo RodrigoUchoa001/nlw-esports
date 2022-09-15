d30cf10cffa70ca62342868908d3364d43763c6a
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-09-15 20:53:27 -0300

    agora puxando os jogos da api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import { GameBanner } from './components/GameBanner';
+import { useState, useEffect } from 'react';
 
 import './styles/main.css';
 import logoImg from './assets/logo-nlw-esports.svg';
 import { CreateAdBanner } from './components/CreateAdBanner';
 
+interface Game{
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
 function App() {
+
+  // por convenção, o nome da função q ta na segunda posição é 
+  // 'setAlgumaCoisa'
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data);
+      setGames(data);
+    })
+  }, []);
+
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} />
@@ -14,7 +38,19 @@ function App() {
       </h1>
       
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <GameBanner bannerUrl='/game-1.png' title='lolzin' adsCount={5}/>
+        {games.map(game => {
+          return (
+            <GameBanner 
+              // key é apenas algo q o react pede pra diferenciar cada elemento,
+              // sem ele funciona mas mostra um erro no console
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          );
+        })}
+
       </div>
 
       <CreateAdBanner />      
