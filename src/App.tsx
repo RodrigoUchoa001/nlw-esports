af3b99b8904ffaefe8cc9966129d53017d6c9e0a
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-09-17 12:15:50 -0300

    formulário de criação de anúncio agr funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { CreateAdBanner } from './components/CreateAdBanner';
 import './styles/main.css';
 import logoImg from './assets/logo-nlw-esports.svg';
 import { CreateAdModal } from './components/CreateAdModal';
+import axios from 'axios';
 
 interface Game{
   id: string;
@@ -23,11 +24,9 @@ function App() {
   const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:3333/games')
-    .then(response => response.json())
-    .then(data => {
-      // console.log(data);
-      setGames(data);
+    axios('http://localhost:3333/games')
+    .then(response => {
+      setGames(response.data);
     })
   }, []);
 
--- src/components/CreateAdModal.tsx
+++ src/components/CreateAdModal.tsx
@@ -3,7 +3,8 @@ import * as Dialog from '@radix-ui/react-dialog';
 import * as Checkbox from '@radix-ui/react-checkbox';
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
 import { Input } from './Form/Input';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, FormEvent } from 'react';
+import axios from 'axios';
 
 interface Game{
     id: string;
@@ -13,15 +14,47 @@ interface Game{
 export function CreateAdModal(){
     const [games, setGames] = useState<Game[]>([]);
     const [weekDays, setWeekDays] = useState<string[]>([]);
+    const [useVoiceChannel, setUseVoiceChannel] = useState(false);
 
     useEffect(() => {
-        fetch('http://localhost:3333/games')
-        .then(response => response.json())
-        .then(data => {
-        // console.log(data);
-        setGames(data);
+        axios('http://localhost:3333/games')
+        .then(response => {
+            setGames(response.data);
         })
     }, []);
+
+    async function handleCreateAd(event: FormEvent){
+        event.preventDefault();
+
+        const formData = new FormData(event.target as HTMLFormElement);
+        const data = Object.fromEntries(formData);
+
+        console.log(data);
+        console.log(weekDays);
+        console.log(useVoiceChannel);
+
+        // validação simples pra n enviar form vazio
+        if (!data.name){
+            return;
+        }
+
+        try {
+            axios.post(`http://localhost:3333/games/${data.game}/ads`,{
+                name: data.name,
+                yearsPlaying: Number(data.yearsPlaying),
+                discord: data.discord,
+                weekDays: weekDays.map(Number),
+                hourStart: data.hourStart,
+                hourEnd: data.hourEnd,
+                useVoiceChannel: useVoiceChannel
+            })
+
+            alert('anúncio criado com sucesso!');
+        }catch(err){
+            console.log(err);
+            alert('erro ao criar p anúncio!');
+        }
+    }
 
     
     return (
@@ -31,11 +64,12 @@ export function CreateAdModal(){
           <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25' >
             <Dialog.Title className='text-3xl font-black'>Publique um anúncio</Dialog.Title>
             
-            <form className='mt-8 flex flex-col gap-4'>
+            <form onSubmit={handleCreateAd} className='mt-8 flex flex-col gap-4'>
               <div className='flex flex-col gap-2'>
                 <label htmlFor="game" className='font-semibold'>Qual o game?</label>
                 <select 
                     id='game' 
+                    name='game'
                     className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none'
                     defaultValue=''
                 >
@@ -50,18 +84,18 @@ export function CreateAdModal(){
 
               <div className='flex flex-col gap-2'>
                 <label htmlFor="name">Seu nome (ou nickgame)</label>
-                <Input id="name" placeholder='Como te chamam dentro do game?'/>
+                <Input name='name' id="name" placeholder='Como te chamam dentro do game?'/>
               </div>
 
               <div className='grid grid-cols-2 gap-6'>
                 <div className='flex flex-col gap-2'>
-                  <label htmlFor="yersPlaying">Joga há quantos anos?</label>
-                  <Input id="yersPlaying" type="number" placeholder='Tudo bem ser ZERO'/>  
+                  <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
+                  <Input name='yearsPlaying' id="yearsPlaying" type="number" placeholder='Tudo bem ser ZERO'/>  
                 </div>
 
                 <div className='flex flex-col gap-2'>
                   <label htmlFor="discord">Qual seu discord?</label>
-                  <Input id="discord" placeholder='Usuario#0000'/>  
+                  <Input name='discord' id="discord" placeholder='Usuario#0000'/>  
                 </div>
               </div>
 
@@ -130,20 +164,31 @@ export function CreateAdModal(){
                 <div className='flex flex-col gap-2 flex-1'>
                   <label htmlFor="hourStart">Qual horário do dia?</label>
                   <div className='grid grid-cols-2 gap-2'>
-                    <Input id='hourStart' type="time" placeholder='De'/> 
-                    <Input id='hourEnd' type="time" placeholder='Até'/> 
+                    <Input name='hourStart' id='hourStart' type="time" placeholder='De'/> 
+                    <Input name='hourEnd' id='hourEnd' type="time" placeholder='Até'/> 
                   </div>
                 </div>
               </div>
 
-              <div className='mt-2 flex items-center gap-2 text-sm'>
-                <Checkbox.Root className='w-6 h-6 p-1 rounded bg-zinc-900'>
+              <label className='mt-2 flex items-center gap-2 text-sm'>
+                <Checkbox.Root 
+                    checked={useVoiceChannel}
+                    onCheckedChange={(checked) => {
+                        if (checked===true){
+                            setUseVoiceChannel(true);
+                        }
+                        else {
+                            setUseVoiceChannel(false);
+                        }
+                    }}
+                    className='w-6 h-6 p-1 rounded bg-zinc-900'
+                >
                     <Checkbox.Indicator>
                         <Check className='w-4 h-4 text-emerald-400'/>
                     </Checkbox.Indicator>
                 </Checkbox.Root>
                 Costumo me conectar ao chat de voz
-              </div>
+              </label>
 
               <footer className='mt-4 flex justify-end gap-4'>
                 <Dialog.Close 
